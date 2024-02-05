29188e8b195368280f317b161ceafbe46dd9bdbf
3Vvv <333vovk@mail.ru>
2024-02-05 15:18:05 +0300

    added base colors

---
+++ FoodDelivery/Resources/AppColors.swift
@@ -0,0 +1,14 @@
+//
+//  AppColors.swift
+//  FoodDelivery
+//
+//  Created by Вячеслав Вовк on 05.02.2024.
+//
+
+import UIKit
+
+enum AppColors {
+    static let accentOrange = UIColor(red: 211/255.0, green: 84/255.0, blue: 0/255.0, alpha: 1.000)
+    static let lightGray = UIColor(red: 236/255.0, green: 240/255.0, blue: 241/255.0, alpha: 1.000)
+    static let gray = UIColor(red: 128/255.0, green: 128/255.0, blue: 128/255.0, alpha: 1.000)
+}
