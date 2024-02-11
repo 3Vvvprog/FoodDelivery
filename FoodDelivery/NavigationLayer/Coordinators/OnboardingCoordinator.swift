f78044c45efc4a3b6902f986829349ee01e224d8
3Vvv <333vovk@mail.ru>
2024-02-11 11:12:12 +0300

    Added Factory

--- FoodDelivery/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ FoodDelivery/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class OnboardingCoordinator: Coordinator {
+    private let factory = SceneFactory.self
+    
     override func start() {
         showOnboarding()
     }
@@ -19,30 +21,7 @@ class OnboardingCoordinator: Coordinator {
 
 private extension OnboardingCoordinator {
     func showOnboarding() {
-        var pages = [UIViewController]()
-        let firstVC = OnboardingPartViewController()
-        firstVC.imageToShow = UIImage.chicken
-        firstVC.titleText = "Delicious Food"
-        firstVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        
-        let secondVC = OnboardingPartViewController()
-        secondVC.imageToShow = UIImage.shipped
-        secondVC.titleText = "Fast Shipping"
-        secondVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum rhoncus nulla."
-        let thirdVC = OnboardingPartViewController()
-        thirdVC.imageToShow = UIImage.medal
-        thirdVC.titleText = "Certificate Food"
-        thirdVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris a id."
-        let fourthVC = OnboardingPartViewController()
-        fourthVC.imageToShow = UIImage.creditCard
-        fourthVC.titleText = "Payment Online"
-        fourthVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui ultricies sit massa."
-        pages.append(firstVC)
-        pages.append(secondVC)
-        pages.append(thirdVC)
-        pages.append(fourthVC)
-        let presenter = OnboardingViewPresenter(coordinator: self)
-        let viewController = OnboardingViewController(pages: pages, viewOutput: presenter)
+        let viewController = factory.makeOnboardingScene(coordinator: self)
         navigationController?.pushViewController(viewController, animated: true)
     }
 }
